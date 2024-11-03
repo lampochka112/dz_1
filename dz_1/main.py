a31f343714c96f18494c0532b88640ce7f759c8a
lampochka112 <daniilgolosov232@gmail.com>
2024-11-03 16:29:24 +0000

    done task 4

--- dz_1/main.py
+++ dz_1/main.py
@@ -1,3 +1,1 @@
-diagonal1 = int(input("введnите первую диагональ:"))
-diagonal2 = int(input("введnите вторую диагональ:"))
-print((diagonal1 * diagonal2) //2)
+print
---
+++ dz_1/task4.py
@@ -0,0 +1,4 @@
+my_list =["to be", "or not", "to be"]
+for line in my_list:
+    print(line.center(len(line)+40))
+    
